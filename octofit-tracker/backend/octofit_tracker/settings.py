9f1ec354e3bb2e916772fae3d04908d3ab51a8aa
JAHESC <jahe@simcorp.com>
2026-02-10 16:09:06 +0000

    Step 4: Setup Django REST Framework, start the server, and test the API

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-ut3r=@a*f&7(*14kj(+h$k&pt2$%(*ibt@#p(c0*tcv%2)fn0d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+codespace_host = f"{CODESPACE_NAME}-8000.app.github.dev" if CODESPACE_NAME else None
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if codespace_host:
+    ALLOWED_HOSTS.append(codespace_host)
 
 
 # Application definition
